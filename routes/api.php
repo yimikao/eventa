c8ce31d3a202b7da00cd12fc9e0ec2c904f36073
Yimika Olotin <holayinkajr@gmail.com>
2021-02-16 18:22:41 +0100

    set up app routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,62 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+//AUTH ROUTES
+Route::post('/user-create', function(Request $request) {
+
+    User::create([
+        "name" =>  $request->input('name'),
+        "email" => $request->input('email'),
+        "password" =>  Hash::make($request->input('password')),
+
+    ]);
 });
+
+Route::post('/login', function(Request $request) {
+    $credentials = [
+        "email" => $request->input('email'),
+        "password" => $request->input('password'),
+
+    ];
+
+    $token = auth('api')->attempt($credentials);
+    return $token;
+});
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    // return $request->user(); this also works
+
+    $user =  auth()->user();
+
+    return $user;
+});
+
+
+
+//VISITOR & USER
+Route::post('/events/{event}/register', [App\Http\Controllers\RegistrationController::class, 'store']);
+Route::get('/events', [App\Http\Controllers\EventController::class, 'index']);
+Route::get('/events/{event}', [App\Http\Controllers\EventController::class, 'show']);
+Route::get('/allevents/{user}', [App\Http\Controllers\UserController::class, 'show']);
+
+// Auth::routes();//Ask a question about this(is it Auth::api()->routes())
+
+
+//USER
+Route::group(['middleware' => 'auth:api', 'prefix'=>'dashboard'], function() {
+    
+    
+    Route::post('/events', [App\Http\Controllers\EventController::class, 'store']);
+    Route::patch('/events/{event}', [App\Http\Controllers\EventController::class, 'update']);
+    Route::delete('/events/{event}', [App\Http\Controllers\EventController::class, 'destroy']);
+
+    Route::get('/registrations', [App\Http\Controllers\UserController::class, 'showRegistrations']);
+
+});
+
+
+
